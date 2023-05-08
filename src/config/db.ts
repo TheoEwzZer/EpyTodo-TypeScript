4e48aed6dda1b5962985788705280c15a8da965b
Théo Fabiano <theo.fabiano@epitech.eu>
2023-05-08 13:59:33 +0200

    🔒 chore: add dotenv package to load environment variables ✨ feat: add Config interface to type-check database configuration 🐛 fix: change connection variable type to Connection from mysql2 package

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,23 @@
-import { QueryError, createConnection } from "mysql2";
+import dotenv from "dotenv";
+import { QueryError, createConnection, Connection } from "mysql2";
 
-const config = {
+dotenv.config();
+
+interface Config {
+  database: string | undefined;
+  host: string | undefined;
+  user: string | undefined;
+  password: string | undefined;
+}
+
+const config: Config = {
   database: process.env.MYSQL_DATABASE,
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_ROOT_PASSWORD,
 };
 
-const connection = createConnection(config);
+const connection: Connection = createConnection(config);
 
 connection.connect((err: QueryError | null): void => {
   if (err) {
